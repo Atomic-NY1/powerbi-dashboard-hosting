9fe3afb6707b341d9c0e8107cc70097701f40b65
Atomic-NY1 <MVNY1@outlook.com>
2025-06-01 16:45:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Scroll-to-top button functionality
+const scrollTopBtn = document.getElementById("scrollTopBtn");
 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > window.innerHeight) {
+    scrollTopBtn.classList.add("show");
+    scrollTopBtn.classList.remove("hide");
+  } else {
+    scrollTopBtn.classList.remove("show");
+    scrollTopBtn.classList.add("hide");
+  }
+});
+
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
